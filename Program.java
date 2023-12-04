4cb364e0a096e36377f0375a1914d97dc0ae8654
DavidAkabogu <Akabogudavid@gmail.com>
2023-12-04 15:47:26 +0100

    intro to java

---
+++ Program.java
@@ -0,0 +1,17 @@
+public class Program {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 91, 4, 5 };
+        System.out.println(getMaxNumber(array));
+    }
+
+    private static int getMaxNumber(int[] array) {
+        int maxNumber = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxNumber) {
+                maxNumber = array[i];
+            }
+        }
+        return maxNumber;
+    }
+}
--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-function getMeanTemp(temperatures) {
-  let sum = 0;
-  for (let i = 0; i < temperatures.length; i++) {
-    sum += temperatures[i];
-  }
-  return sum / temperatures.length;
-}
-day1 = [
-  12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21,
-  20, 19, 17, 16,
-];
-day2 = [
-  17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24,
-  21, 19, 18, 17, 16,
-];
-// debugger;
-console.log(`mean: ${getMeanTemp(day1)}`);
+// function getMeanTemp(temperatures) {
+//   let sum = 0;
+//   for (let i = 0; i < temperatures.length; i++) {
+//     sum += temperatures[i];
+//   }
+//   return sum / temperatures.length;
+// }
+// day1 = [
+//   12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21,
+//   20, 19, 17, 16,
+// ];
+// day2 = [
+//   17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24,
+//   21, 19, 18, 17, 16,
+// ];
+// // debugger;
+// console.log(`mean: ${getMeanTemp(day1)}`);
+
+// // [1,3,2,4,5,3,2,91]
+// function getMaxNumberInArray(array) {
+//   // create a variable to hold the max number
+//   let maxNumber = array[0];
+
+//   // loop through the array
+//   // for every iteration, check if the current number is greater than the maximum number
+//   for (let i = 1; i < array.length; i++) {
+//     if (array[i] > maxNumber) {
+//       maxNumber = array[i];
+//     }
+//   }
+//   return maxNumber;
+//   // if the current number is greater than the maximum number, update the maximum number
+// }
+
+// arrayOfInts = [1, 3, 2, 4, 5, 3, 2, 91];
+// console.log(getMaxNumberInArray(arrayOfInts));
+
+// let i = 10
+// console.log(--i)
+// console.log(i)
