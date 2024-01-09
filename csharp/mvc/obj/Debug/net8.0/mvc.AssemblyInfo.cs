e3a72839d553ca50e140b1774b3fb433dcf5e119
DavidAkabogu <Akabogudavid@gmail.com>
2024-01-09 10:03:36 +0100

    update

--- csharp/mvc/obj/Debug/net8.0/mvc.AssemblyInfo.cs
+++ csharp/mvc/obj/Debug/net8.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35871f9f586b8bda839039191efee56e4b6933f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0390cee9e71975aabd0889e6b46ad3a86274e7da")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
