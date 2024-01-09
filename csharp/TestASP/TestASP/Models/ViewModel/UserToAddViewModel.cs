5ea09a74008f6d624ca6b3fc68e6406e12d9a2a2
DavidAkabogu <Akabogudavid@gmail.com>
2024-01-09 12:15:58 +0100

    new project

---
+++ csharp/TestASP/TestASP/Models/ViewModel/UserToAddViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TestASP.Models.ViewModel
+{
+	public class UserToAddViewModel
+	{
+		public UserToAddViewModel()
+		{
+		}
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
