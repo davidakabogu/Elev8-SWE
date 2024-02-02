44d306b4b5b8f7c5088cc9b1913353448f1ccc2e
DavidAkabogu <Akabogudavid@gmail.com>
2024-02-02 14:07:27 +0100

    including images

--- flutterlayoutdemo/lib/main.dart
+++ flutterlayoutdemo/lib/main.dart
@@ -27,17 +27,22 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: const Column(
-        children: [
-          TitleSection(
-              name: 'Oeschinen Lake Campground',
-              location: 'Kandersteg, Switzerland')
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageSection(
+                  image: 'images/9215c3fae621312e8640fc6e12dd32c3.jpeg'),
+              TitleSection(
+                  name: 'Oeschinen Lake Campground',
+                  location: 'Kandersteg, Switzerland'),
+              ButtonSection(),
+              TextSection()
+            ],
+          ),
+        ));
   }
 }
 
@@ -78,6 +83,76 @@ class TitleSection extends StatelessWidget {
           const Text('41'),
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ButtonWithText(color: color, icon: Icons.call, label: 'PHONE'),
+        ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
+        ButtonWithText(color: color, icon: Icons.share, label: 'SHARE')
+      ],
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText(
+      {super.key,
+      required this.color,
+      required this.icon,
+      required this.label});
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, color: color),
+        Text(label, style: TextStyle(color: color))
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(32.0),
+      child: Column(children: [
+        Text(
+            'To follow Flutter best practices, create one class, or Widget, to contain each part of your layout. When Flutter needs to re-render part of a UI, it updates the smallest part that changes. This is why Flutter makes “everything a widget”. If only the text changes in a Text widget, Flutter redraws only that text. Flutter changes the least amount of the UI possible in response to user input.'),
+      ]),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
     );
   }
 }
